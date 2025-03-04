a6861d56e3cee799e0d53fa0246b165b6fa60d1c
ksahana <k_sahana@trimble.com>
2025-03-04 22:19:36 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config()
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    return res.send("Hello world connected to GymApplication, Developed by Harsha & Sahana");
+})
+
+app.use("/api/customers", require("./routes/customerRoute"));
+app.use("/api/subscriptions", require("./routes/subscriptionRoute"));
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
