b3b7fb575d7363d0426bc9888e170f71610cf3aa
ksahana <k_sahana@trimble.com>
2025-03-08 17:55:21 +0530

    Initialized routes for customerSubscriptions

--- controllers/customerSubscriptionController.js
+++ controllers/customerSubscriptionController.js
@@ -1,17 +1,21 @@
-const addCustomerSubscription = async(req,res)=>{
-    
+const addCustomerSubscription = async (req, res) => {
+    return res.status(201).json({ message: "CustomerSubscription addded Successfully" })
 }
-const updateCustomerSubscription = async(req,res)=>{
-    
+const updateCustomerSubscription = async (req, res) => {
+    return res.status(200).json({ message: "CustomerSubscription Updated Successfully" })
 }
-const getCustomerSubscription = async(req,res)=>{
-    
+const getCustomerSubscription = async (req, res) => {
+    return res.status(200).json({ message: "CustomerSubscription fetched Successfully" })
 }
-const getAllCustomerSubscription = async(req,res)=>{
-    
+const getAllCustomerSubscription = async (req, res) => {
+    return res.status(201).json({ message: "CustomerSubscriptions fetched for all customers Successfully" })
 }
-const deleteCustomerSubscription = async(req,res)=>{
-    
+const deleteCustomerSubscription = async (req, res) => {
+    return res.status(204).json({ message: "CustomerSubscription deleted Successfully" })
 }
 
-module.exports={updateCustomerSubscription, getAllCustomerSubscription, getCustomerSubscription, addCustomerSubscription, deleteCustomerSubscription}
+const getAllCustomerSubscriptionforCustomer = async (req, res) => {
+    return res.status(200).json({ message: "CustomerSubscriptions fetched for a customer  Successfully" })
+}
+
+module.exports = { updateCustomerSubscription, getAllCustomerSubscription, getCustomerSubscription, addCustomerSubscription, deleteCustomerSubscription, getAllCustomerSubscriptionforCustomer }
--- routes/customerSubscriptionRoute.js
+++ routes/customerSubscriptionRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { getAllCustomerSubscription, getCustomerSubscription, deleteCustomerSubscription, updateCustomerSubscription, addCustomerSubscription } = require("../controllers/customerSubscriptionController")
+const { getAllCustomerSubscription, getCustomerSubscription, deleteCustomerSubscription, updateCustomerSubscription, getAllCustomerSubscriptionforCustomer, addCustomerSubscription } = require("../controllers/customerSubscriptionController")
 const { deleteSubscription } = require("../controllers/subscriptionController")
 const { verifyAdmin } = require("../middleware/checkCustomer")
 
@@ -8,6 +8,7 @@ const router = express.Router()
 route.post('/', verifyAdmin, addCustomerSubscription)
 route.put('/:id', verifyAdmin, updateCustomerSubscription)
 route.get('/:id', getCustomerSubscription)
+route.get('/:custId', getAllCustomerSubscriptionforCustomer)
 route.get('/', getAllCustomerSubscription)
 route.delete('/:id', deleteSubscription)
 
