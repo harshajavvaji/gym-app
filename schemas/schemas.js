be0c1b7e5ab7c6fb177df689b53fdd5d9a05aa05
ksahana <k_sahana@trimble.com>
2025-04-18 00:05:09 +0530

    added cusotmerActivity model

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -40,4 +40,35 @@ class CustomerSubscription {
     price // price
 }
 
-module.exports = { Subscription, Customer, CustomerSubscription }
+class Activity {
+    type
+    calories
+}
+
+class CustomerActivity {
+    constructor(id, customerId, activities = [], date, inTime, outTime, weight = null) {
+        this.id = id;
+        this.customerId = customerId;
+        this.activities = activities;  // Changed from activity:[Activity] to activities
+        this.date = date;
+        this.inTime = inTime;
+        this.outTime = outTime;
+        this.weight = weight;
+    }
+}
+
+// Usage example:
+// const activity1 = new Activity('running', 500);
+// const activity2 = new Activity('weightlifting', 300);
+// const customerActivity = new CustomerActivity(
+//     '1',
+//     'customer123',
+//     [activity1, activity2],
+//     new Date(),
+//     '09:00',
+//     '10:00',
+//     70
+// );    
+
+
+module.exports = { Subscription, Customer, CustomerSubscription, CustomerActivity }
